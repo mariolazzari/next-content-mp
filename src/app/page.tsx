6d8eb415f73ab02d48f554c0bb29262863d1459a
Mario Lazzari <mario.lazzari@gmail.com>
2023-07-27 20:41:31 +0200

    feat: form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,44 @@
+"use client";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useForm } from "react-hook-form";
 
 export default function Home() {
+  const form = useForm<{
+    name: string;
+    email: string;
+  }>();
+
   return (
     <main className="">
+      <Form {...form}>
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </Form>
+
       <Label htmlFor="name">Name</Label>
       <Input id="name" name="name" />
     </main>
